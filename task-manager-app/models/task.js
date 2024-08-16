97d9d016c85f33f8302e72d22ebb6bf17bd11d86
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-08-15 20:45:28 -0400

    task schema

---
+++ task-manager-app/models/task.js
@@ -0,0 +1,17 @@
+import moongose from "mongoose"
+
+const taskSchema = new moongose.Schema({
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
+});
+
+const Task = moongose.models.Taks || moongose.model('Task', taskSchema);
+
+export default Task;
