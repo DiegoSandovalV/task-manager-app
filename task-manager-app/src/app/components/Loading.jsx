948a66885613463b1d8351f78aa257f8fa38bbe4
Diego <sandoval.vargas.diego.1c@gmail.com>
2024-08-19 20:54:55 -0400

    Updated styles in loading screen

--- task-manager-app/src/app/components/Loading.jsx
+++ task-manager-app/src/app/components/Loading.jsx
@@ -8,14 +8,30 @@ export default function Loading() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        height: "100vh",
-        flexDirection: "column",
+        height: "94vh",
+        p: 2,
       }}
     >
-      <CircularProgress />
-      <Typography variant="h4" sx={{ mt: 2 }}>
-        Loading...
-      </Typography>
+      <Box
+        sx={{
+          padding: 3,
+          borderRadius: 3,
+          boxShadow: 5,
+          maxWidth: 600,
+          width: "100%",
+          minHeight: 500,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: 2,
+          backgroundColor: "white",
+        }}
+      >
+        <CircularProgress />
+        <Typography variant="h4" sx={{ mt: 2 }}>
+          Loading...
+        </Typography>
+      </Box>
     </Box>
   )
 }
